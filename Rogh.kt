0b16c8520c148cc3477157ec9ed6c1bb0ab59eb4
aaditya8008 <114769448+aaditya8008@users.noreply.github.com>
2023-06-26 20:25:23 +0530

    forgot

--- Rogh.kt
+++ Rogh.kt
@@ -1,41 +1,25 @@
 fun main() {
-    val circle=Circle(4.0)
-    val player=Player("Smiley")
-    if(circle is Circle){               //typechecking
-        println("This is Circle")
+   
+}
+ open class A{                  //public
+public var a=10             
+private var b=20              //only this class
+internal var c=30             
+protected var d=40            //sub classes
+
+
+
+}
+
+ class B:A(){          
+    fun test(){
+        println(a)
+        println(b)
+        println(c)
+        println(d)
+
     }
-    val arr=arrayOf(circle,player)
-    for(i in arr){
-        if(i is Circle){
-            println(i.area())
-        }
-        else{
-            (i as Player).saymyname()
-        }
-    }
-}
-interface Draggable {
-    fun drag()
+
 }
 
-abstract class shape : Draggable { // same like classes but no bracket
-
-    abstract fun area(): Double
-}
-
-class Circle(val radii: Double) : shape() {
-    override fun area(): Double {
-        return Math.PI * radii * radii
-    }
-    override fun drag() {
-        println("Circle is Dragging")
-    }
-}
-
-
-
-class Player(val name: String) : Draggable {
-    override fun drag() = println("$name is dragging")
-fun saymyname()=println("Hey my name id - $name")
-}
 
